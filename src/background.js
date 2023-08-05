713296adc05eceab89b2dd7ada9999f80a0e48b1
Dries Bosmans <bosmans_dries@hotmail.com>
2023-08-05 19:33:00 +0200

    background

--- src/background.js
+++ src/background.js
@@ -1,13 +1,27 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  var width = 1024;
-  var height = 512;
+const person = {
+  foreName: "Dries",
+  surName: "Bosmans",
+  duration: getCurrentAge(),
+  whereAbouts: "Tessenderlo",
+  lineOfWork: "Software developer",
+  page: "https://dries.bosmans.dev/",
+  mainLanguages: ["C#", "JavaScript"],
+  diversions: ["Natuurpunt Ham", "dog", "coding"],
+};
 
-  chrome.app.window.create('app.html', {
-    id: 'appWindow1',
-    bounds: {
-      width: width,
-      height: height,
-    },
-    resizable: false
-  });
-});
+const dog = {
+  name: "Olaf",
+  breed: "Belgian Malinois",
+  numberOfPaws: 4
+};
+console.table(person);
+
+function getCurrentAge() {
+  const DOB = new Date(1990, 4, 28);
+  // get time difference
+  const diff = Date.now() - DOB.getTime();
+  // convert the calculated difference in date format
+  const year = new Date(diff).getUTCFullYear();
+  // now calculate the age of the user
+  return Math.abs(year - 1970);
+}
